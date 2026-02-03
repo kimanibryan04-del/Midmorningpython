a2ac39dac617b5f4658529f566d5d2fe86eb3c73
kimanibryan04-del <kimanibryan04@gmail.com>
2026-02-03 13:21:27 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display an output
+print("Welcome to Python Programming")
+print("Bryan")
+print(200)
+print(40*3)
+
---
+++ variables.py
@@ -0,0 +1,17 @@
+# Variable is a container used to store a value
+
+school = "eMobilis"
+firstname = "John"
+lastname = "Smith"
+num1 = "45"
+num2 = "100"
+num3 = "145"
+language = "Python"
+
+print(school)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(school,"is a coding school")
+print(language,"is a web programming language")
+print("The sum of",num1,"and",num2,"is",num3)
