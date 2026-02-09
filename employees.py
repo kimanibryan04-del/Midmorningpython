a749f88fa5aadbd3914a3f928f2e6fc0f454e925
kimanibryan04-del <kimanibryan04@gmail.com>
2026-02-09 11:30:28 +0300

    new commit

--- employees.py
+++ employees.py
@@ -1,15 +1,3 @@
-
-number = int(input("Enter a number: "))
-
-
-if number  == 0:
-    print("The number is neutral.")
-elif number % 2 == 0:
-    print("The number is even.")
-else:
-    print("The number is odd.")
-
-
 def display_employee_info(fullname, position, age, gender):
     print(f"Name: {fullname}")
     print(f"Position: {position}")
