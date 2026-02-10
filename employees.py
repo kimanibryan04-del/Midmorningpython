a703f5c19819fa06537a0609f024d94bed61a571
kimanibryan04-del <kimanibryan04@gmail.com>
2026-02-10 11:16:30 +0300

    Add files via upload

---
+++ employees.py
@@ -0,0 +1,14 @@
+def display_employee_info(fullname, position, age, gender):
+    print(f"Name: {fullname}")
+    print(f"Position: {position}")
+    print(f"Age: {age}")
+    print(f"Gender: {gender}")
+    print("-" * 30)
+
+print("--- eMobilis Employee Details ---\n")
+
+display_employee_info("John Doe", "Software Instructor", 28, "Male")
+display_employee_info("Jane Smith", "Project Manager", 32, "Female")
+display_employee_info("Alex Mwangi", "Technical Lead", 35, "Male")
+display_employee_info("Sarah Adhiambo", "Data Analyst", 26, "Female")
+display_employee_info("David Otieno", "System Admin", 30, "Male")
